d42db460acf9c2fa17033a5ff47801350ca97be3
“crustyjuggler” <damonscott01@gmail.com>
2017-11-27 14:50:04 -0500

    added code to actually check if a product is available

--- src/vendingMachine.js
+++ src/vendingMachine.js
@@ -1,44 +1,52 @@
-const validCoins = [
+const VALIDCOINS = [
     { type: "quarter", value: 0.25 },
     { type: "dime", value: 0.10 },
     { type: "nickel", value: 0.05 }
 ]
 
-const isValidCoin = (coin) => {
-    return validCoins.find((validCoin) => {
-        return validCoin.type === coin
-    }) || false
-}
+const PRODUCTS = [
+    { type: "cola", price: 1.0 },
+    { type: "chips", price: 0.5 },
+    { type: "candy", price: 0.65 }
+]
 
 let total
-const resetTotal = () => {
-    total = 0
+const resetTotal = (reset) => {
+    if (reset) total = 0
+}
+
+const isValidCoin = (coin) => {
+    return VALIDCOINS.find((validCoin) => {
+        return validCoin.type === coin
+    }) || false
 }
 
 const formatCurrency = (number) => {
     return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(number)
 }
 
+const insertCoin = (coin, reset) => {
+    resetTotal(reset)
+    let value = isValidCoin(coin).value || false
+    if (value) {
+        total += value
+    }
+    return value
+}
+
+const vend = (product) => {
+    return PRODUCTS.find((listProduct) => {
+        return listProduct.type === product
+    }) || false
+}
+
 const display = () => {
     return total > 0 ? formatCurrency(total) :  'INSERT COINS'
 }
 
-const vend = (product) => {
-    return true
-}
-
 const vendingMachine = {
-    InsertCoin : (coin, reset) => {
-        if (reset) resetTotal()
-        let value = isValidCoin(coin).value || false
-        if (value) {
-            total += value
-        }
-        return value
-    },
-
+    InsertCoin : insertCoin,
     Vend : vend,
-
     Display : display
 }
 
